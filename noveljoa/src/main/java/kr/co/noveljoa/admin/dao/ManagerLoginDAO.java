39502d5428d3f530a5fcc2ca7cf1f6165d64899f
2jaehoon <dlwogns4521@naver.com>
2023-05-17 01:20:28 +0900

    재훈 오라클 maven 및 서버 컨텍스트 설정 base-package="kr.co.noveljoa.*" 및 프로퍼티즈 설정 서버 매핑*.do로도 설정 매니저 selectdao 추가 05-17 01:20 재훈 커밋

--- noveljoa/src/main/java/kr/co/noveljoa/admin/dao/ManagerLoginDAO.java
+++ noveljoa/src/main/java/kr/co/noveljoa/admin/dao/ManagerLoginDAO.java
@@ -1,45 +1,45 @@
 package kr.co.noveljoa.admin.dao;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
+
+import org.apache.ibatis.session.SqlSession;
 
 import kr.co.noveljoa.admin.domain.MLoginDomain;
 import kr.co.noveljoa.admin.vo.MLoginVO;
-import kr.co.noveljoa.conn.DbConnection;
 
 public class ManagerLoginDAO {
 	
 	public MLoginDomain selectLogin(MLoginVO mVO) throws SQLException {
 		MLoginDomain mlDomain = null;  
+		//MyBatis 얻기
+		SqlSession ss = MyBatisHandler.getInstance().getMyBatisHandler(false);
 		
-		Connection con = null;
-		PreparedStatement pstmt = null;
-		ResultSet rs = null;
-		DbConnection db = DbConnection.getInstance();
+		//transaction완료
+		mlDomain=ss.selectOne("kr.co.noveljoa.admin.Mapper.selectLogin", mVO);
 		
-		try {
-			con=db.getConn();
-			
-			String select = "select id, Cdate from minfo2 where id = ? and password = ?";
-			pstmt = con.prepareStatement(select);
-			pstmt.setString(1, mVO.getId());
-			pstmt.setString(2, mVO.getPassword());
-			
-			
-			rs=pstmt.executeQuery();
-			
-			if(rs.next() ) {
-				mlDomain = new MLoginDomain(rs.getString("id"), rs.getDate("Cdate"));
-			}
-			
-		}finally {
-			db.dbClose(rs, pstmt, con);
-		}
-		
+		ss.commit();
+		//연결 끊기
+				if(ss != null) {
+					ss.close();
+				}//end if
+
 		
 		return mlDomain;
 	}//selectLogin
+	
+	public static void main(String[] args) {
+		
+		ManagerLoginDAO mDAO = new ManagerLoginDAO();
+		 MLoginVO m = new MLoginVO("manager", "1234");
+		 try {
+			 MLoginDomain md  = mDAO.selectLogin(m);
+			System.out.println(md);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		
+	}
+	
 
 }//class
