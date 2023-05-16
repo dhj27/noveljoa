3bafccf2fc6f8fc247cc0b11236b22b3332b0b2d
user <user@DESKTOP-COJ9858>
2023-05-16 17:39:36 +0900

    0516 에피소드 commit

--- noveljoa/src/main/java/kr/co/noveljoa/user/episode/controller/EpController.java
+++ noveljoa/src/main/java/kr/co/noveljoa/user/episode/controller/EpController.java
@@ -2,6 +2,7 @@ package kr.co.noveljoa.user.episode.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 import kr.co.noveljoa.user.episode.vo.EpCheckVO;
 import kr.co.noveljoa.user.episode.vo.EpListVO;
